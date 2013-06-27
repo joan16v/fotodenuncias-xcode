e2868cf9bede7e032bc24daececee991e0362bd4
joan16v <joan16v@gmail.com>
2013-06-27 09:33:38 +0200

    update

--- foto denuncias/fotoDenunciasViewController.h
+++ foto denuncias/fotoDenunciasViewController.h
@@ -8,40 +8,58 @@
 
 #import <UIKit/UIKit.h>
 #import <MobileCoreServices/MobileCoreServices.h>
+#import <CoreLocation/CoreLocation.h>
+
 
 @interface fotoDenunciasViewController : UIViewController 
 <UIImagePickerControllerDelegate, 
-UINavigationControllerDelegate>
+UINavigationControllerDelegate,
+UIAlertViewDelegate>
 {
 
     IBOutlet UITextField *descripcion;
     IBOutlet UIButton *elijeFoto;
     IBOutlet UIButton *hazFoto;    
+    IBOutlet UIButton *infoweb;    
     IBOutlet UIImageView *logo;
     UIImageView *imageView;
     IBOutlet UIButton *subir;
     IBOutlet UIButton *twitter;    
-    
+    IBOutlet CLLocationManager *locationManager;    
+    UIImage *imageGlobal;
+    UIAlertView *baseAlert;    
     BOOL newMedia;
-    NSString *pathImagen;
-    BOOL imagenElegida;
+    BOOL imagenElegida;    
+    //NSString *pathImagen;
+    NSString *idrespuesta;   
+    
+    //AFHTTPRequestOperation *operation;
 
 }
 
 @property (nonatomic, retain) IBOutlet UITextField *descripcion;
 @property (nonatomic, retain) IBOutlet UIButton *elijeFoto;
 @property (nonatomic, retain) IBOutlet UIButton *hazFoto;
-@property (nonatomic, retain) UIImageView *imageView;
+@property (nonatomic, retain) IBOutlet UIButton *infoweb;
 @property (nonatomic, retain) UIImageView *logo;
+@property (nonatomic, retain) UIImageView *imageView;
 @property (nonatomic, retain) IBOutlet UIButton *subir;
 @property (nonatomic, retain) IBOutlet UIButton *twitter;
+@property (nonatomic, retain) IBOutlet CLLocationManager *locationManager;
+@property (nonatomic, retain) UIImage *imageGlobal;
+@property (nonatomic, retain) UIAlertView *baseAlert;
+@property (nonatomic) BOOL newMedia;
+@property (nonatomic) BOOL imagenElegida;
+//@property (nonatomic, retain) NSString *pathImagen;
+@property (nonatomic, retain) NSString *idrespuesta;
 
 - (IBAction)useCamera;
 - (IBAction)useCameraRoll;
 - (IBAction)subirDenuncia;
 - (IBAction)abrirTwitter;
 - (IBAction)abrirWeb;
-
 - (IBAction)textFieldReturn:(id)sender;
+- (IBAction)showProgress:(id)sender;
+- (IBAction)showProgressDismiss;
 
 @end
--- foto denuncias/fotoDenunciasViewController.m
+++ foto denuncias/fotoDenunciasViewController.m
@@ -7,7 +7,13 @@
 //
 
 #import "fotoDenunciasViewController.h"
+//#import "AFHTTPClient.h"
+#import "AFHTTPRequestOperation.h"
+//#import "AFImageRequestOperation.h"
+//#import "../Reachability.h"
 //#import "ASIFormDataRequest.h"
+#import "SVProgressHUD.h"
+#include <math.h>
 
 @interface fotoDenunciasViewController ()
 
@@ -15,16 +21,36 @@
 
 @implementation fotoDenunciasViewController
 
-@synthesize descripcion, elijeFoto, hazFoto, subir;
-//@dynamic imageView;
-@synthesize imageView, twitter, logo;
+@synthesize descripcion; 
+@synthesize elijeFoto; 
+@synthesize hazFoto; 
+@synthesize infoweb; 
+@synthesize logo;
+@synthesize imageView;
+@synthesize subir; //@dynamic imageView;
+@synthesize twitter;
+@synthesize locationManager;
+@synthesize imageGlobal;
+@synthesize baseAlert;
+@synthesize newMedia;
+@synthesize imagenElegida;
+//@synthesize pathImagen;
+@synthesize idrespuesta;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    imagenElegida=FALSE;
+    imagenElegida=FALSE; //UIImage *imageGlobal=nil;
+    idrespuesta=@"-";
+    //AFHTTPRequestOperation *operation;
+    
+    //geolocalizacion
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.distanceFilter = kCLDistanceFilterNone; // whenever we move
+    locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters; // 100 m
+    [locationManager startUpdatingLocation];
     
 }
 
@@ -49,7 +75,7 @@
         imagePicker.delegate = self;
         imagePicker.sourceType = 
         UIImagePickerControllerSourceTypeCamera;
-        //imagePicker.mediaTypes = [NSArray arrayWithObjects:(NSString *) kUTTypeImage,nil];
+        imagePicker.mediaTypes = [NSArray arrayWithObjects:(NSString *) kUTTypeImage,nil];
         imagePicker.allowsEditing = NO;
         [self presentModalViewController:imagePicker 
                                 animated:YES];
@@ -68,58 +94,39 @@
         imagePicker.delegate = self;
         imagePicker.sourceType = 
         UIImagePickerControllerSourceTypePhotoLibrary;
-        //imagePicker.mediaTypes = [NSArray arrayWithObjects:(NSString *) kUTTypeImage,nil];
+        imagePicker.mediaTypes = [NSArray arrayWithObjects:(NSString *) kUTTypeImage,nil];
         imagePicker.allowsEditing = NO;
-        [self presentModalViewController:imagePicker animated:YES];
-        //imagePicker release];
+        [self presentModalViewController:imagePicker animated:YES]; //imagePicker release];
         newMedia = NO;
     }
 }
 
-
-
-
 -(void)imagePickerController:(UIImagePickerController *)picker
 didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
-    //NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];    
-   
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];       
     [self dismissModalViewControllerAnimated:YES];
-    //if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
-    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
     
-    //image.
-    //UIImagePickerControllerOriginalImage.description;
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
+        
+        UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];    
+        //imageGlobal=image; 
+        imageGlobal = [self resizeImage:image];
+        //UIImagePickerControllerOriginalImage.description;    
+        //NSURL* localUrl = (NSURL *)[info valueForKey:UIImagePickerControllerReferenceURL];
+        //NSString* theActualPath = [localUrl path];
+        imageView.image = imageGlobal;    
+        if (newMedia) UIImageWriteToSavedPhotosAlbum(image, self,@selector(image:finishedSavingWithError:contextInfo:),nil);    
+        imagenElegida=TRUE;
+        //pathImagen=theActualPath;
     
-    //NSString *pipi=(@"%@",UIImagePickerControllerOriginalImage.image);
-    
-    NSURL* localUrl = (NSURL *)[info valueForKey:UIImagePickerControllerReferenceURL];
-    NSString *filePath= [localUrl absoluteString];
-    
-    /*UIAlertView *alert = [[UIAlertView alloc]
-                          initWithTitle: @"pipi"
-                          message: filePath\
-                          delegate: nil
-                          cancelButtonTitle:@"OK"
-                          otherButtonTitles:nil];
-    [alert show];  */  
-    
-    imageView.image = image;
-    
-    //imageView.
-    //image;
-    
-    if (newMedia) UIImageWriteToSavedPhotosAlbum(image, self,@selector(image:finishedSavingWithError:contextInfo:),nil);
-    
-    imagenElegida=TRUE;
-    pathImagen=filePath;
-    
-    //}
-    //else if ([mediaType isEqualToString:(NSString *)kUTTypeMovie])
-    //{
+    }
+    else if ([mediaType isEqualToString:(NSString *)kUTTypeMovie])
+    {
      //Code here to support video if enabled
-	//}
+	}
 }
+
 -(void)image:(UIImage *)image
 finishedSavingWithError:(NSError *)error 
  contextInfo:(void *)contextInfo
@@ -131,8 +138,7 @@ finishedSavingWithError:(NSError *)error
                               delegate: nil
                               cancelButtonTitle:@"OK"
                               otherButtonTitles:nil];
-        [alert show];
-        //[alert release];
+        [alert show]; //[alert release];
     }
 }
 
@@ -147,6 +153,148 @@ finishedSavingWithError:(NSError *)error
 
 -(void)abrirWeb{
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"http://www.fotodenuncias.net"]];
+}
+
+// Displays our progress indicator with a message
+- (IBAction)showProgress {
+    
+    // initialize our Alert View window without any buttons
+    baseAlert=[[UIAlertView alloc]initWithTitle:@"Enviando foto-denuncia..." 
+                                                     message:nil 
+                                                    delegate:self
+                                           cancelButtonTitle:nil 
+                                           otherButtonTitles:nil];    
+    // Display our Progress Activity view
+    [baseAlert show];
+    
+    // create and add the UIActivity Indicator
+    UIActivityIndicatorView *activityIndicator=[[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    activityIndicator.center=CGPointMake(baseAlert.bounds.size.width/2.0f,baseAlert.bounds.size.height-40.0f);
+    
+    // initialize to tell our activity to start animating.
+    [activityIndicator startAnimating];
+    [baseAlert addSubview:activityIndicator]; //[activityIndicator release];
+    
+    // automatically close our window after 3 seconds has passed.
+    [self performSelector:@selector(showProgressDismiss)
+               withObject:nil afterDelay:0.0f];
+}
+
+// Delegate to dismiss our Activity indicator after the number of seconds has passed.
+- (void) showProgressDismiss
+{
+    NSLog(@"entrado en el delegado del alert progress");
+    //durante 30 segundos revisar si se ha subido la foto denuncia.
+    /*NSInteger maxtry=30;
+    NSInteger i=0;
+    while (i<maxtry) {
+        NSLog(@"entrado en while de comprobacion idrespuesta");
+        //NSLog(idrespuesta);
+        sleep(1);
+        if([idrespuesta isEqualToString:@"-"]) {        
+        } else {
+            NSString *trimmedString = [idrespuesta stringByTrimmingCharactersInSet:[NSCharacterSet decimalDigitCharacterSet]];            
+            if([trimmedString length])
+            {
+                NSLog(@"some characters outside of the decimal character set found");
+                //ko
+                [baseAlert dismissWithClickedButtonIndex:0 animated:NO]; 
+                //error: intentalo mas tarde
+                UIAlertView *alert = [[UIAlertView alloc]
+                                      initWithTitle: @"Error"
+                                      message: @"Error subiendo denuncia. Vuelve a intentarlo."\
+                                      delegate: nil
+                                      cancelButtonTitle:@"OK"
+                                      otherButtonTitles:nil];
+                [alert show];  
+            }
+            else
+            {
+                NSLog(@"all characters were in the decimal character set");
+                //ok
+                [baseAlert dismissWithClickedButtonIndex:0 animated:NO]; 
+                // open a alert with an OK and cancel button
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Completado" message:@"Foto-denuncia publicada!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:@"Ver denuncia", nil];
+                [alert show]; //[alert release];        
+                
+            }            
+        }
+        i++;
+    }    
+    //ko
+    [baseAlert dismissWithClickedButtonIndex:0 animated:NO]; 
+    //error: intentalo mas tarde
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle: @"Error"
+                          message: @"Error subiendo denuncia. Tiempo máximo de espera alcanzado. Revisa la conexión a Internet!."\
+                          delegate: nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil];
+    [alert show]; */
+}        
+
+- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    // the user clicked one of the OK/Cancel buttons
+    if (buttonIndex == 0)
+    {
+        NSLog(@"ok");
+        descripcion.text=@"";
+    }
+    else
+    {
+        NSLog(@"ver"); //[self abrirWeb];
+        NSString *urlweb=@"http://www.fotodenuncias.net/foto.php";
+        urlweb = [urlweb stringByAppendingString:@"?denuncia="];        
+        urlweb = [urlweb stringByAppendingString:idrespuesta];        
+        urlweb = [urlweb stringByAppendingString:@"&desc=denuncia"];        
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString: urlweb]];
+    }
+}
+
+-(UIImage *)resizeImage:(UIImage *)image {
+	
+	CGImageRef imageRef = [image CGImage];
+	CGImageAlphaInfo alphaInfo = CGImageGetAlphaInfo(imageRef);
+	CGColorSpaceRef colorSpaceInfo = CGColorSpaceCreateDeviceRGB();
+	
+	if (alphaInfo == kCGImageAlphaNone)
+		alphaInfo = kCGImageAlphaNoneSkipLast;
+	
+	int width, height;
+	
+	width = 640;
+	height = 480;
+	
+	CGContextRef bitmap;
+	
+	if (image.imageOrientation == UIImageOrientationUp | image.imageOrientation == UIImageOrientationDown) {
+		bitmap = CGBitmapContextCreate(NULL, width, height, CGImageGetBitsPerComponent(imageRef), CGImageGetBytesPerRow(imageRef), colorSpaceInfo, alphaInfo);		
+	} else {
+		bitmap = CGBitmapContextCreate(NULL, height, width, CGImageGetBitsPerComponent(imageRef), CGImageGetBytesPerRow(imageRef), colorSpaceInfo, alphaInfo);		
+	}	
+	if (image.imageOrientation == UIImageOrientationLeft) {
+		NSLog(@"image orientation left");
+		CGContextRotateCTM (bitmap,M_PI/2);
+		CGContextTranslateCTM (bitmap, 0, -height);		
+	} else if (image.imageOrientation == UIImageOrientationRight) {
+		NSLog(@"image orientation right");
+		CGContextRotateCTM (bitmap, (M_PI/2)*(-1));
+		CGContextTranslateCTM (bitmap, -width, 0);		
+	} else if (image.imageOrientation == UIImageOrientationUp) {
+		NSLog(@"image orientation up");			
+	} else if (image.imageOrientation == UIImageOrientationDown) {
+		NSLog(@"image orientation down");	
+		CGContextTranslateCTM (bitmap, width,height);
+		CGContextRotateCTM (bitmap, M_PI*(-1));		
+	}	
+	CGContextDrawImage(bitmap, CGRectMake(0, 0, width, height), imageRef);
+	CGImageRef ref = CGBitmapContextCreateImage(bitmap);
+	UIImage *result = [UIImage imageWithCGImage:ref];
+	
+	CGContextRelease(bitmap);
+	CGImageRelease(ref);
+	
+	return result;	
 }
 
 - (void) subirDenuncia
@@ -173,30 +321,114 @@ finishedSavingWithError:(NSError *)error
             
             //subir la fotodenuncia
             
-            /*NSString *urlpost=@"http://www.fotodenuncias.net/iphone/iphonePost.php";
-            NSURL *fileURLpost = [NSURL fileURLWithPath:urlpost];
-            ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:fileURLpost];
-            [request addPostValue:descripcion.text forKey:@"desc"];
-            [request addPostValue:@"GPS" forKey:@"gps"];
-            [request addFile:@"/Users/ben/Desktop/ben.jpg" forKey:@"photos"];
-            [request addData:imageView withFileName:@"pic.jpg" andContentType:@"image/jpeg" forKey:@"photo"];            */
+            subir.enabled=NO;
+            elijeFoto.enabled=NO;
+            hazFoto.enabled=NO;
+            descripcion.enabled=NO;
+            infoweb.enabled=NO;
+            twitter.enabled=NO;
+            [SVProgressHUD show];  
             
+            //preparamos coordenadas
+            NSString *coordenadas=[NSString stringWithFormat:@"%f", locationManager.location.coordinate.latitude];
+            coordenadas = [coordenadas stringByAppendingString:@","];
+            coordenadas = [coordenadas stringByAppendingString:[NSString stringWithFormat:@"%f", locationManager.location.coordinate.longitude]];            
             
+            NSString *urlfull=@"http://www.fotodenuncias.net/iphone/iphonePost.php";
+            NSURL *url = [NSURL URLWithString:urlfull];            
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];            
+            [request setHTTPMethod:@"POST"];      
             
+            NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+            NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
+            [request addValue:contentType forHTTPHeaderField:@"Content-Type"];            
             
-            //vaciar descripcion y imagen
-            imagenElegida=FALSE;
-            pathImagen=@"";
-            [imageView removeFromSuperview];
-            descripcion.text=@"";
+            NSMutableData *postBody = [NSMutableData data];
+           
+            [postBody appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+            [postBody appendData:[@"Content-Disposition: form-data; name=\"desc\"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+            [postBody appendData:[descripcion.text dataUsingEncoding:NSUTF8StringEncoding]];            
             
-            UIAlertView *alert = [[UIAlertView alloc]
-                                  initWithTitle: @"OK"
-                                  message: @"Foto-denuncia subida en fotodenuncias.net!"\
-                                  delegate: nil
-                                  cancelButtonTitle:@"OK"
-                                  otherButtonTitles:nil];
-            [alert show];                
+            [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+            [postBody appendData:[@"Content-Disposition: form-data; name=\"coord\"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+            [postBody appendData:[coordenadas dataUsingEncoding:NSUTF8StringEncoding]];                          
+            
+            //reescalado de la imagen: para que no ocupe demasiado
+            //UIImage *img = [self resizeImage:imageGlobal];
+            UIImage *img = imageGlobal;
+            
+            NSData *imageData = UIImageJPEGRepresentation(img, 70);
+            [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+            
+            //le ponemos un nombre aleatorio a la imagen
+            NSInteger rand=arc4random();
+            NSString *nameimg=@"Content-Disposition: form-data; name=\"uploadedfile\"; filename=\"iphonepic";
+            NSString *string = [NSString stringWithFormat:@"%d", rand];            
+            nameimg = [nameimg stringByAppendingString:string];
+            nameimg = [nameimg stringByAppendingString:@".jpg\"\r\n"];
+            
+            [postBody appendData:[[NSString stringWithString:nameimg] dataUsingEncoding:NSUTF8StringEncoding]];
+            [postBody appendData:[[NSString stringWithString:@"Content-Type: image/jpg\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+            [postBody appendData:[NSData dataWithData:imageData]];   
+            [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+            [request setHTTPBody:postBody];
+            
+            AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];            
+            [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {                  
+               
+                NSLog(@"operation hasAcceptableStatusCode: %d", [operation.response statusCode]);                
+                NSLog(@"response string: %@ ", operation.responseString);    
+                idrespuesta=operation.responseString;
+                
+                [SVProgressHUD dismiss];                
+                
+                if([idrespuesta isEqualToString:@"-"]) {        
+                } else {
+                    NSString *trimmedString = [idrespuesta stringByTrimmingCharactersInSet:[NSCharacterSet decimalDigitCharacterSet]];            
+                    if([trimmedString length])
+                    {
+                        //ko
+                        NSLog(@"some characters outside of the decimal character set found");
+                        [baseAlert dismissWithClickedButtonIndex:0 animated:NO]; 
+                        //error: intentalo mas tarde
+                        UIAlertView *alert = [[UIAlertView alloc]
+                                              initWithTitle: @"Error"
+                                              message: @"Error subiendo denuncia. Vuelve a intentarlo."\
+                                              delegate: nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+                        [alert show]; 
+                        subir.enabled=YES;
+                        elijeFoto.enabled=YES;
+                        hazFoto.enabled=YES;
+                        descripcion.enabled=YES;    
+                        infoweb.enabled=YES;
+                        twitter.enabled=YES;                        
+                    }
+                    else
+                    {
+                        //ok
+                        NSLog(@"all characters were in the decimal character set");
+                        [baseAlert dismissWithClickedButtonIndex:0 animated:NO]; 
+                        // open a alert with an OK and cancel button
+                        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Completado" message:@"Foto-denuncia publicada!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:@"Ver denuncia", nil];
+                        [alert show]; //[alert release];                                
+                        subir.enabled=YES;
+                        elijeFoto.enabled=YES;
+                        hazFoto.enabled=YES;
+                        descripcion.enabled=YES;    
+                        infoweb.enabled=YES;
+                        twitter.enabled=YES;                        
+                    }            
+                }                                
+                
+            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {                
+                NSLog(@"error: %@", operation.responseString);                
+            }];       
+
+            [operation start];      
+            
+            //[SVProgressHUD show];  
             
         }       
     }
